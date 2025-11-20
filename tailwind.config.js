26ef570fb6e6d120cc53d180ad6b1841bce63e1e
osamagivegh-hash <osamagivegh@gmail.com>
2025-11-20 20:44:10 +0300

    Revamp landing page layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./app/components/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brandBlue: "#1E3A8A",
+        brandLight: "#DBEAFE",
+        brandGray: "#F3F4F6",
+      },
+    },
   },
   plugins: [],
-}
-
+};
